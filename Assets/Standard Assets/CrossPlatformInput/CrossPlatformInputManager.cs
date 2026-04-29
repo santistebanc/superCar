6635bbf0f602748626720bbdfc98aae0210a1075
santistebanc <santistebanc94@gmail.com>
2026-04-30 00:22:15 +0200

    Add missing CrossPlatformInput scripts to fix namespace error

---
+++ Assets/Standard Assets/CrossPlatformInput/CrossPlatformInputManager.cs
@@ -0,0 +1,54 @@
+using System;
+using UnityEngine;
+
+namespace UnityStandardAssets.CrossPlatformInput
+{
+    public static class CrossPlatformInputManager
+    {
+        private static bool m_IsMobileInput = false;
+
+        public static void SetMobileInput(bool mobile)
+        {
+            m_IsMobileInput = mobile;
+        }
+
+        public static float GetAxis(string axisName)
+        {
+            if (m_IsMobileInput)
+            {
+                return Input.GetAxis(axisName);
+            }
+
+            string mobileAxis = "Mouse X";
+            if (axisName == "Horizontal")
+            {
+                return Input.GetAxis("Horizontal");
+            }
+            else if (axisName == "Vertical")
+            {
+                return Input.GetAxis("Vertical");
+            }
+            else if (axisName == "Jump")
+            {
+                return Input.GetButton("Jump") ? 1f : 0f;
+            }
+
+            return Input.GetAxis(axisName);
+        }
+
+        public static bool GetButton(string buttonName)
+        {
+            return Input.GetButton(buttonName);
+        }
+
+        public static bool GetButtonDown(string buttonName)
+        {
+            return Input.GetButtonDown(buttonName);
+        }
+
+        public static bool GetButtonUp(string buttonName)
+        {
+            return Input.GetButtonUp(buttonName);
+        }
+    }
+}
